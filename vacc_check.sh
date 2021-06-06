8a254a83cdd88192b31c1f8c8c853db3e68cffa9
Bharath <53872268+Bharys@users.noreply.github.com>
2021-06-06 13:11:02 +0530

    Update vacc_check.sh

--- vacc_check.sh
+++ vacc_check.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-res=$(python3 ~/cowin_check_email.py)
+res=$(python3 ~/covid_vacc_slot_enquiry.py)
 echo "$res" > ~/result.txt
+res=$(python3 ~/cowin_check_email.py)
+echo "$res" >> ~/result.txt
